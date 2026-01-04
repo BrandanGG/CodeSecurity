2864c519ea11180329a00939ad5213ca5fca9e78
BrandanGG <101829241+BrandanGG@users.noreply.github.com>
2026-01-03 19:13:38 -0500

    init of db functions

---
+++ devTools/osv_ingest/db.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+TODO: Add the database connection
+TODO: Add the database schema
+TODO: Add the database functions
+TODO: Add the database queries
+TODO: Add the database transactions
+Flow: DB Connection -> All other functions
+*/
